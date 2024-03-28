c7de2ad37a44d793ac32691ded5c4ce96328968b
GithubCDCode <162576004+GithubCDCode@users.noreply.github.com>
2024-03-28 23:15:12 +0800

    Update and rename edger差异表达.R to edger.R

--- edger.R
+++ edger.R
@@ -1,4 +1,4 @@
-library("edgeR")#3.24.3   版本可能也会有影响
+library("edgeR")#3.24.3   
 library('gplots')
 foldchange=1
 padj=0.05
@@ -7,7 +7,7 @@ exp=as.matrix(rt)
 dimnames=list(rownames(exp),colnames(exp))
 data=matrix(as.numeric(as.matrix(exp)),nrow=nrow(exp),dimnames = dimnames)
 #data=data[rowMeans(data)>1,]
-data=data[rowSums(data)>1,]#数据筛选阈值不一致
+data=data[rowSums(data)>1,]
 
 group <- read.csv("ground.txt",header=TRUE,row.names = 1,check.names = FALSE)
 group <- group[,1]
@@ -28,11 +28,11 @@ diff=allDiff
 newData=y$pseudo.counts
 
 write.csv(diff, "CRNF.csv")
-diffSig = diff[(diff$FDR < padj & (diff$logFC>foldchange | diff$logFC<(-foldchange))),]#筛选有显著差异的#
-write.table(diffSig, file="CRNFdiffSig.xls",sep="\t",quote=F)#输出有显著差异表达的到diffSig这个文件#
+diffSig = diff[(diff$FDR < padj & (diff$logFC>foldchange | diff$logFC<(-foldchange))),]
+write.table(diffSig, file="CRNFdiffSig.xls",sep="\t",quote=F)
 write.csv(diffSig, "CRNFdiffSig.csv")
-diffUp = diff[(diff$FDR < padj & (diff$logFC>foldchange)),]#foldchange>0是上调，foldchange<0是下调#
-write.table(diffUp, file="CRNFup.xls",sep="\t",quote=F)#39-42把上调和下调分别输入up和down两个文件#
+diffUp = diff[(diff$FDR < padj & (diff$logFC>foldchange)),]
+write.table(diffUp, file="CRNFup.xls",sep="\t",quote=F)
 write.csv(diffUp, "CRNFdiffUp.csv")
 diffDown = diff[(diff$FDR < padj & (diff$logFC<(-foldchange))),]
 write.table(diffDown, file="CRNFdown.xls",sep="\t",quote=F)
